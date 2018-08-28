d26439195a050633325fc79f0db8499bdf28cca3
zhangbin5 <zhangbin5@crop.neatease.com>
2018-08-28 15:00:39 +0800

    Initial commit

---
+++ src/main/java/com/example/test/bean/User.java
@@ -0,0 +1,37 @@
+package com.example.test.bean;
+
+
+
+
+
+public class User {
+    private String userName;
+    private String sex;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    private Integer age;
+
+}
